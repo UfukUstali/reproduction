f6afb4389a0c9fca571249247d86a9a0402f1d35
Ufuk Ustali <ustaliufuk73@gmail.com>
2023-11-23 23:50:46 +0100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@hebilicious/server-block-nuxt"],
+  debug: true,
+  devtools: { enabled: true },
+});
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<server lang="ts">
+const message = "Hello World!!!";
+const bye = "bye!";
+export const GET = defineEventHandler(() => ({ message }));
+export const POST = defineEventHandler(() => ({ message: bye }));
+</server>
+
+<script lang="ts" setup>
+const { data } = await useFetch("/api/index");
+async function func() {
+  console.log(await $fetch("/api/index", { method: "POST" }));
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ data }}</p>
+    <button @click="func">click</button>
+  </div>
+</template>
